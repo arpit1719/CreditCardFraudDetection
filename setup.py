2c8129d1d8f0527433855cd34b52f2e2b8ad0bed
Arpit <arpitnimkar2021@gmail.com>
2024-01-19 21:17:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="CreditCardFraudDetection",
+    version="0.0.1",
+    author="Arpit",
+    author_email="arpitnimkar2020@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
